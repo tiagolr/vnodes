17fc906852e4662651ff96f504bad6745a0646f5
txlabs <txlabs@protonmail.com>
2021-05-16 16:15:24 +0100

    comments

--- src/components/Screen.vue
+++ src/components/Screen.vue
@@ -51,12 +51,16 @@ export default {
       this.panzoom.zoom(scale)
       this.panzoom.pan( x,y )
     },
+    /**
+     * Centers and zooms a rectangle
+     * @param rect { left, right, top, bottom }
+     * @param scale force zoom to a specific value (eg: 1)
+     */
     zoomRect (rect, scale=null) {
       const screen = this.$refs.screen
       const width = rect.right - rect.left
       const height = rect.bottom - rect.top
       if (!scale || typeof scale !== 'number') {
-        // calculate scale to fit contents
         const dx = width / screen.clientWidth
         const dy = height / screen.clientHeight
         scale = 1 / Math.max(dx, dy)
@@ -69,17 +73,17 @@ export default {
     },
     zoomNode (node) {
       const screen = this.$refs.screen
-      const sepX = screen.clientWidth / 2 - node.width / 2
-      const sepY = screen.clientHeight / 2 - node.height / 2
+      const marginX = screen.clientWidth / 2 - node.width / 2
+      const marginY = screen.clientHeight / 2 - node.height / 2
 
       this.zoomRect({
-        left: node.x - sepX,
-        right: node.x + node.width + sepX,
-        top: node.y - sepY,
-        bottom: node.y + node.height + sepY
+        left: node.x - marginX,
+        right: node.x + node.width + marginX,
+        top: node.y - marginY,
+        bottom: node.y + node.height + marginY
       })
     },
-    panNode (node, offsetX=0, offsetY=0) {
+    panNode (node, offsetX=0, offsetY=0) { // centers node on screen
       const zoom = this.panzoom.getZoom()
       const x = this.$el.clientWidth / 2 - (node.x + node.width / 2) * zoom + offsetX
       const y = this.$el.clientHeight / 2 - (node.y + node.height / 2) * zoom + offsetY
