d8be0ce4969fedba697e63c1f43f9eb3c351546a
txlabs <txlabs@protonmail.com>
2021-06-21 22:51:16 +0100

    pages

--- src/demo/Demo.vue
+++ src/demo/Demo.vue
@@ -2,23 +2,26 @@
   <div style="margin-top: 50px; margin-bottom: 50px">
     <h1>Vnodes</h1>
 
-    <h2>Sink</h2>
+    <h2>
+      Sink <a href="https://github.com/txlabs/vnodes/blob/master/src/demo/Sink.vue" target="_blank">&lt;&gt;</a>
+    </h2>
     <sink>
     </sink>
 
-    <h2>Edit</h2>
+    <h2>Edit <a href="https://github.com/txlabs/vnodes/blob/master/src/demo/Edit.vue" target="_blank">&lt;&gt;</a></h2>
     <edit>
     </edit>
 
-    <h2>Ports</h2>
+
+    <h2>Ports <a href="https://github.com/txlabs/vnodes/blob/master/src/demo/Ports.vue" target="_blank">&lt;&gt;</a></h2>
     <ports>
     </ports>
 
-    <h2>Labels</h2>
+    <h2>Labels <a href="https://github.com/txlabs/vnodes/blob/master/src/demo/Labels.vue" target="_blank">&lt;&gt;</a></h2>
     <labels>
     </labels>
 
-    <h2>Benchpress</h2>
+    <h2>Benchpress <a href="https://github.com/txlabs/vnodes/blob/master/src/demo/Benchmark.vue" target="_blank">&lt;&gt;</a></h2>
     <benchpress>
     </benchpress>
   </div>
@@ -40,6 +43,15 @@ export default {
   },
 }
 </script>
+
+<style lang="stylus" scoped>
+h2
+  width: calc(100% - 225px)
+  a
+    font-family: monospace;
+    font-size: 1em;
+    float: right
+</style>
 
 <style>
 h2 {
